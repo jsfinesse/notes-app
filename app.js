a9bc6a460deddececb4ac453f4a10988168af740
Prithu Singh <prithusingh0327@gmail.com>
2022-03-17 19:03:41 +0530

    Added add command

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const chalk = require("chalk").default;
 const yargs = require("yargs");
 
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 
 yargs.version("1.0.1");
 
@@ -21,8 +21,7 @@ yargs.command({
         },
     },
     handler: function (argv) {
-        console.log("Title: " + argv.title);
-        console.log("Body: " + argv.body);
+        notes.addNote(argv.title, argv.body);
     },
 });
 
--- notes.js
+++ notes.js
@@ -1,5 +1,43 @@
+const fs = require("fs");
+
 const getNotes = () => {
     return "Your notes...";
+};
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+
+    const duplicateNotes = notes.filter(note => note.title === title);
+
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title,
+            body,
+        });
+    
+        saveNotes(notes);
+    }
+    else {
+        console.log("Duplicate");
+    }
+};
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync("notes.json", dataJSON);
 }
 
-module.exports = getNotes;
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync("notes.json");
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+};
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+};
